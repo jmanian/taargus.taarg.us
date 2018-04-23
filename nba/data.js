fbc04254acffb2e38f3115f4b891b5a02538830e
Jeff Manian <jeffrey.manian@gmail.com>
2018-04-22 23:26:55 -0400

    update data

--- nba/data.js
+++ nba/data.js
@@ -336,7 +336,8 @@ var rounds = [
           {
             date: '2018-04-22',
             time: '8:30',
-            network: 'TNT'
+            network: 'TNT',
+            winner: 'cle'
           },
           {
             date: '2018-04-25',
