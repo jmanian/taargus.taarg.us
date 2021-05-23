7ea293a3b2112798ee668c790b516325da5520c5
Jeff Manian <jeffrey.manian@gmail.com>
2021-05-23 11:24:51 -0400

    whitespace

--- nba/components/today.js
+++ nba/components/today.js
@@ -1,9 +1,9 @@
 var todayTemplate = `
 <div>
-<h3>Today's Games</h3>
-<div class='row'>
-  <n-today-game v-for='game in games' :game='game'></n-today-game>
-</div>
+  <h3>Today's Games</h3>
+  <div class='row'>
+    <n-today-game v-for='game in games' :game='game'></n-today-game>
+  </div>
 </div>
 `
 
