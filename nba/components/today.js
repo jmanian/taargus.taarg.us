ea3b8980c1034b7da8bc96f6ab81c4277060455d
Jeff Manian <jeffrey.manian@gmail.com>
2024-05-07 22:31:00 -0400

    redo spacing

--- nba/components/today.js
+++ nba/components/today.js
@@ -1,6 +1,6 @@
 const todayTemplate = `
 <template v-if='visible'>
-  <h3><span class="badge text-bg-secondary shadow">Today's Games</span></h3>
+  <h3 class='mt-4 mb-3'><span class="badge text-bg-secondary shadow">Today's Games</span></h3>
   <div class='row row-cols-1 row-cols-md-2 row-cols-xl-3 row-cols-xxl-4'>
     <n-today-game v-for='game in games' :game='game'></n-today-game>
   </div>
